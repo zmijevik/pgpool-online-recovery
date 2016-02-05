9298e9b4aca078acb82e7f01cb04728e3736ffec
zmijevik <persie.joseph@gmail.com>
2016-02-05 22:25:01 +0000

    Only perform when failover is active

--- server-pgpool/etc/pgpool-II/pgpool-failover.sh
+++ server-pgpool/etc/pgpool-II/pgpool-failover.sh
@@ -4,14 +4,15 @@ NODE_ISACTIVE=$(/etc/pgpool-II/node_isactive)
 
 if [ $NODE_ISACTIVE = "false" ]; then
     n=0
+
     while [ $n -lt 10 ]
     do
         python /usr/local/bin/assign-ip $DO_FAILOVER_IP_PGPOOL $ID && break
         n=$((n+1))
         sleep 3
     done
+
+    /etc/pgpool-II/nodes_assurehealthy
+
+    /usr/local/bin/mandrill $SYSADMIN_EMAIL "PgPool Failover Alert:`hostname`" "`hostname` is now the new pgpool server."
 fi
-
-/etc/pgpool-II/nodes_assurehealthy
-
-/usr/local/bin/mandrill $SYSADMIN_EMAIL "PgPool Failover Alert:`hostname`" "`hostname` is now the new pgpool server."
